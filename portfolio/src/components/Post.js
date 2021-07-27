a78005408d6f38247a79232262ded5b2ac59935d
Alex Shaw <alexandergshaw@gmail.com>
2021-07-26 22:13:07 -0500

    wip fix: cors error with posts, moving onto projects

--- portfolio/src/components/Post.js
+++ portfolio/src/components/Post.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function Post() {
   const [postData, setPost] = useState(null);
 
+  //   todo: debug CORS error (@ 1:04:56)
   useEffect(() => {
     sanityClient
       .fetch(
@@ -34,27 +35,28 @@ export default function Post() {
           Welcome to my page of blog posts
         </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {postData && postData.map((post, index) => (
-            <article>
-              <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                <span
-                  className="block h64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
-                  key={index}
-                >
-                  <img
-                    src={post.mainImage.asset.url}
-                    alt={post.mainImage.alt}
-                    className="w-full h-full rounded-r object-cover absolute"
-                  />
-                  <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                    <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
-                      {post.title}
-                    </h3>
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span
+                    className="block h64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    key={index}
+                  >
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="w-full h-full rounded-r object-cover absolute"
+                    />
+                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                      <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                        {post.title}
+                      </h3>
+                    </span>
                   </span>
-                </span>
-              </Link>
-            </article>
-          ))}
+                </Link>
+              </article>
+            ))}
         </div>
       </section>
     </main>
