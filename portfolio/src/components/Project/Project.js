24131d2eb463d66fcf3f34b592b799fb5dfde37b
Alex Shaw <alexandergshaw@gmail.com>
2021-07-27 19:07:10 -0500

    style: add in terminal background

--- portfolio/src/components/Project/Project.js
+++ portfolio/src/components/Project/Project.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import sanityClient from "../client.js";
+import sanityClient from "../../client.js";
 
 export default function Project() {
   const [projectData, setProjectData] = useState(null);
