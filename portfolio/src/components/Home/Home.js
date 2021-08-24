656f3472d04223989b5f525b0cefe04837755e98
Alex Shaw <alexandergshaw@gmail.com>
2021-08-23 21:38:43 -0500

    wip feat: capturing keydown events

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Home.css";
-import Typical from "react-typical";
 
 export default function Home() {
   // const handleKeyPress = (event) => {
   //   console.log('event', event);
   // };
 
-  let input = document.createElement("span");
-	input.setAttribute("id", "input");
+  let input = 'test';
 
   let handleInput = (e) => {
     console.log('event', e);
+    input += e.key;
+    console.log('input', input);
   }
 
   return (
@@ -23,10 +23,10 @@ export default function Home() {
           wrapper="p"
           className="TerminalText"
         /> */}
+        <span>{input}</span>
         <span className="Cursor" >|</span>
       </div>
 
-      <div className="ScanLine"></div>
       {/* <img src={firBackgroundPicture} alt="Water Droplets on Fir" className="absolute object-cover w-full h-full"/>
             <section class="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello there!</h1>
