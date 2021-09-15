2a2905c1016cbd45cfc60f77bc061e7fa0e42932
Alex Shaw <alexandergshaw@gmail.com>
2021-09-14 21:54:24 -0500

    wip feat: play key noises on keypress

--- portfolio/src/common/constants/keys.constants.js
+++ portfolio/src/common/constants/keys.constants.js
@@ -1,3 +1,4 @@
 export const BACKSPACE_KEY_CODE = 8;
+export const SPACE_KEY_CODE = 32;
 export const LEFT_ARROW_KEY_CODE = 37;
 export const RIGHT_ARROW_KEY_CODE = 39;
--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BACKSPACE_KEY_CODE, LEFT_ARROW_KEY_CODE, RIGHT_ARROW_KEY_CODE } from "../../common/constants/keys.constants";
+import {
+  BACKSPACE_KEY_CODE,
+  LEFT_ARROW_KEY_CODE,
+  RIGHT_ARROW_KEY_CODE,
+  SPACE_KEY_CODE,
+} from "../../common/constants/keys.constants";
 import "./Home.css";
 
 class Home extends Component {
@@ -10,11 +15,13 @@ class Home extends Component {
       input: "",
       cursorPosition: 0,
       stringBeforeCursor: "",
-      stringAfterCursor: ""
+      stringAfterCursor: "",
     };
   }
   handleInput(newInput) {
-    console.log('newInput', newInput);
+    let audio = new Audio('../../assets/sound/keys.mp3');
+    audio.play();
+
     if (newInput.key.length === 1) {
       const newStringOnScreen = this.state.input + newInput.key;
       const newCursorPosition = this.state.cursorPosition + 1;
@@ -22,36 +29,58 @@ class Home extends Component {
       this.setState({
         input: newStringOnScreen,
         stringBeforeCursor: newStringOnScreen.slice(0, newCursorPosition),
-        cursorPosition: newCursorPosition
+        cursorPosition: newCursorPosition,
       });
     } else if (newInput.keyCode === BACKSPACE_KEY_CODE) {
       this.setState({
         stringBeforeCursor: this.state.stringBeforeCursor.slice(0, -1),
-        cursorPosition: this.state.cursorPosition > 0 ? this.state.cursorPosition - 1 : 0
+        cursorPosition:
+          this.state.cursorPosition > 0 ? this.state.cursorPosition - 1 : 0,
       });
-    } 
-    
-    else if (newInput.keyCode === LEFT_ARROW_KEY_CODE) {
-      const newCursorPosition = this.state.cursorPosition > 0 ? this.state.cursorPosition - 1 : 0;
-      const newStringBeforeCursor = this.state.input.slice(0, newCursorPosition);
+    } else if (newInput.keyCode === LEFT_ARROW_KEY_CODE) {
+      const newCursorPosition =
+        this.state.cursorPosition > 0 ? this.state.cursorPosition - 1 : 0;
+      const newStringBeforeCursor = this.state.input.slice(
+        0,
+        newCursorPosition
+      );
       const newStringAfterCursor = this.state.input.slice(newCursorPosition);
 
       this.setState({
         stringBeforeCursor: newStringBeforeCursor,
         stringAfterCursor: newStringAfterCursor,
-        cursorPosition: newCursorPosition
+        cursorPosition: newCursorPosition,
       });
     } else if (newInput.keyCode === RIGHT_ARROW_KEY_CODE) {
-      const newCursorPosition = this.state.cursorPosition < this.state.input.length ? this.state.cursorPosition + 1 : this.state.cursorPosition;
-      const newStringBeforeCursor = this.state.input.slice(0, newCursorPosition);
+      const newCursorPosition =
+        this.state.cursorPosition < this.state.input.length
+          ? this.state.cursorPosition + 1
+          : this.state.cursorPosition;
+      const newStringBeforeCursor = this.state.input.slice(
+        0,
+        newCursorPosition
+      );
       const newStringAfterCursor = this.state.input.slice(newCursorPosition);
 
       this.setState({
         stringBeforeCursor: newStringBeforeCursor,
         stringAfterCursor: newStringAfterCursor,
-        cursorPosition: newCursorPosition
+        cursorPosition: newCursorPosition,
       });
-    } 
+    } else if (newInput.keyCode === SPACE_KEY_CODE) {
+
+      const newCursorPosition = this.state.cursorPosition + 1;
+      const newStringBeforeCursor = this.state.stringBeforeCursor + " ";
+      const newStringOnScreen = this.state.stringBeforeCursor + " " + this.state.stringAfterCursor;
+      
+      console.log('newStringBeforeCursor', newStringBeforeCursor);
+
+      this.setState({
+        input: newStringOnScreen,
+        stringBeforeCursor: newStringBeforeCursor,
+        cursorPosition: newCursorPosition,
+      });
+    }
   }
   // const handleKeyPress = (event) => {
   //   console.log('event', event);
@@ -66,26 +95,27 @@ class Home extends Component {
   // };
 
   render() {
-    let {stringBeforeCursor, stringAfterCursor} = this.state;
-    console.log('stringBeforeCursor', stringBeforeCursor);
-    console.log('stringAfterCursor', stringAfterCursor);
+    let { stringBeforeCursor, stringAfterCursor } = this.state;
 
     const renderStringBeforeCursor = () => {
       if (stringBeforeCursor.length) {
-          return <span className="Terminal-Text">{this.state.stringBeforeCursor}</span>;
-      } 
-    }
+        return (
+          <span className="Terminal-Text">{this.state.stringBeforeCursor}</span>
+        );
+      }
+    };
 
     const renderStringAfterCursor = () => {
       if (stringAfterCursor.length) {
-        return <span className="Terminal-Text">{this.state.stringAfterCursor}</span>;
+        return (
+          <span className="Terminal-Text">{this.state.stringAfterCursor}</span>
+        );
       }
-    }
-
+    };
 
     return (
       <main className="Terminal" onKeyDown={this.handleInput} tabIndex={-1}>
-        <div className="TerminalLines">          
+        <div className="TerminalLines">
           {renderStringBeforeCursor()}
           <span className="Cursor">|</span>
           {renderStringAfterCursor()}
