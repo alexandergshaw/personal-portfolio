e12cdada3ddd85096366c6fce879378cc83d54de
Alex Shaw <alexandergshaw@gmail.com>
2021-11-27 20:39:22 -0600

    feat: add in renewal of token

--- portfolio/src/components/Login/Login.js
+++ portfolio/src/components/Login/Login.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { GoogleLogin } from "react-google-login";
 
 const clientId =
   "682901403391-g02emol5efu4armjqhfddav5to40ab2d.apps.googleusercontent.com";
 
+export const refreshTokenSetup = (res) => {
+  let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+
+  const refreshToken = async () => {
+    const newAuthRes = await res.reuploadAuthResponse();
+    refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+
+    console.log("newAuthRest", newAuthRes);
+    console.log("new auth token", newAuthRes.id_token);
+
+    setTimeout(refreshToken, refreshTiming);
+  };
+
+  setTimeout(refreshToken, refreshTiming);
+};
+
 function Login() {
   const onSuccess = (res) => {
     console.log("[Login Success] res:", res);
+    refreshTokenSetup(res);
   };
 
   const onFailure = (res) => {
